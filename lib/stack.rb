6e3322f9586f80ef0fb41de02b337f25ed371ad5
Hillary Muller <hillaryaward@gmail.com>
2023-02-24 13:44:42 -0600

    commit tree 53ad9d3d0dffbb66a6f653f698f569e87f8a6127

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,32 @@
 # your code here
+class Stack
+    def initialize(limit = nil)
+        @stack = []
+        @limit = limit
+    end
+    def push(value)
+        raise 'Stack Overflow' if full?
+        @stack.push(value)
+    end
+    def pop
+        @stack.pop
+    end
+    def peek
+        @stack.last
+    end
+    def size
+        @stack.length
+    end
+    def empty?
+        @stack.empty?
+    end
+    def full?
+        @limit && @stack.size == @limit
+    end
+    def search(target)
+        @stack.each_with_index do |item, idx|
+            return size - idx -1 if item == target
+        end
+        -1
+    end
+end
